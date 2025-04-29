233aa9ee5e275b55988c331e3f578043f1a6b818
vivianeor <viviane.oliveira280@hotmail.com>
2025-04-29 20:48:46 -0300

    ajustes na calculadora

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 function calculateExpression(expression) {
     try {
-        const sanitizedExpression = sanitizedExpression(expression);
+        const sanitized = sanitizedExpression(expression);
         //eval calcula a expressão
-        const result = eval(sanitizedExpression);
+        const result = eval(sanitized);
 
         if (isNaN(result)) {
             throw new Error('Result is not a number');
@@ -10,9 +10,10 @@ function calculateExpression(expression) {
 
         return result;
     } catch (error) {
-        throw new Error('Invalid mathematical expression.');
+        throw error; // Agora vamos propagar o erro original
     }
 }
+
 // remove qualquer coisa que não seja número, parêntese ou operador.
 function sanitizedExpression(expression) {
     // Permite apenas números, operadores matemáticos, espaços e parênteses
@@ -22,7 +23,7 @@ function sanitizedExpression(expression) {
         throw new Error('Expression contains invalid characters.');
     }
 
-    return sanitized;
+    return expression.trim();
 }
 
 export { calculateExpression };
